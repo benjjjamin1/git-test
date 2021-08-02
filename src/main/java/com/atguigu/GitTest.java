3172919d782d7d69220c06a665ca6a9d821f7bd9
ben <1141814852@qq.com>
2021-08-02 11:49:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author ben
+ * @create 2021 08 02 11:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello get!");
+    }
+}
